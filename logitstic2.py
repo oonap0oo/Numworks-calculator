8c04c31a2907b6a9d0dd2fde152daebb83bfba00
oonap0oo <vnap0v@gmail.com>
2025-04-30 18:55:39 +0200

    Add files via upload

---
+++ logitstic2.py
@@ -0,0 +1,22 @@
+from math import *
+import kandinsky
+# bifurcation diagram 
+# of logistic map
+def logistic_map(astart=3.5,aend=4):
+  aspan=aend-astart
+  stap=4
+  for x in range(320):
+    a=astart+aspan*x/319
+    l=0.5
+    y=[0]*240
+    for i in range(1500):
+      l=a*l*(1-l)
+      oldy=y[int(l*239)]
+      y[int(l*239)]=stap+oldy  
+    for r in range(240):
+      if y[239-r]>255:
+        y[239-r]=255
+      co=kandinsky.color(y[239-r],y[239-r],y[239-r])
+      kandinsky.set_pixel(x,r,co)
+
+logistic_map()
