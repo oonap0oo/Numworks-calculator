7600dfdcc7f06cbfa58dc8c650b39982d41a871f
oonap0oo <vnap0v@gmail.com>
2025-05-16 01:01:41 +0200

    Add files via upload

---
+++ julia.py
@@ -0,0 +1,13 @@
+from kandinsky import *
+co=complex(-0.5125,0.5213)
+for x in range(320):
+  re=(x-160)/160*1.5
+  for y in range(210):
+    im=(y-105)/105
+    z=complex(re,im)
+    c=co
+    i=255
+    while abs(z)<2 and i>0:
+      z=z**2+c
+      i-=1
+    set_pixel(x,y,color(i,i,i))
