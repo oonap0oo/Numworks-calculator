0263eea6abebcc5722d4de2de70647fb17ce6c98
oonap0oo <vnap0v@gmail.com>
2025-05-31 13:54:06 +0200

    Add files via upload

--- line3d.py
+++ line3d.py
@@ -14,6 +14,19 @@ def wave(t):
   x=sin(6*t)*exp(-t**2/20); y=cos(6*t)*exp(-t**2/20); z=t
   return x,y,z
 
+# function draws a line using kandinsky library
+# start point:(x1,y1), end point:(x2,y2), optional color
+def connect(x1,y1,x2,y2,co="black"):
+  xspan=x2-x1;yspan=y2-y1
+  steps=max(abs(xspan),abs(yspan))
+  if steps==0:
+    kandinsky.set_pixel(x1,y1,co)
+    return
+  dx=xspan/steps;dy=yspan/steps
+  x=x1;y=y1
+  for k in range(steps):
+    kandinsky.set_pixel(int(x+.5),int(y+.5),co)
+    x+=dx;y+=dy
 
 # simple projection 3d to 2d
 def proj(x,y,z,a,b):
@@ -34,7 +47,7 @@ def line(fun,trange,xrange,yrange,zrange,txt=""):
   kandinsky.fill_rect(0,0,320,240,"black")
   hoek=pi/12
   a=cos(hoek);b=sin(hoek)
-  Nt=5000
+  Nt=3000
   for k in range(Nt):
     t=trange[0]+(k/(Nt-1)*(trange[1]-trange[0]))
     x,y,z=fun(t)
@@ -48,7 +61,10 @@ def line(fun,trange,xrange,yrange,zrange,txt=""):
     yscr=int(230-230*yproj)
     c=int(y*220+60)
     c=max(0,min(255,c))
-    kandinsky.set_pixel(xscr,yscr,kandinsky.color(c,c,c))
+    if k==0:
+        xscr_old,yscr_old=xscr,yscr
+    connect(xscr_old,yscr_old,xscr,yscr,kandinsky.color(c,c,c))
+    xscr_old,yscr_old=xscr,yscr
   kandinsky.draw_string(txt,30,10,"white","black")
     
 # calling the plotting function
@@ -57,7 +73,10 @@ def line(fun,trange,xrange,yrange,zrange,txt=""):
 #line(sphere,(-10,10),(-10,10),(-10,10),(-10,10),"sphere")
 line(wave,(-10,10),(-1,1),(-1,1),(-10,10),"wave")
 
-
-
+    
+    
+     
+# only for running under Cpython:
+kandinsky.mainloop()
 
 
